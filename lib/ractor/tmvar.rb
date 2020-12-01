7074e0610f6bcbe9d384a8ae30ba04588c21ec74
yoshitsugu <yoshitsugu@users.noreply.github.com>
2020-12-01 20:35:09 +0900

    Use same name as Haskell's TMVar

--- lib/ractor/tmvar.rb
+++ lib/ractor/tmvar.rb
@@ -6,48 +6,121 @@ class Ractor
   # TMVar for Ractor inspired by Haskell's TMVar based on Ractor::TVar.
   class TMVar
     #
-    # Represents "blank" value for TMVar
+    # represents "empty" value for TMVar
     #
-    BLANK = :RACTOR_TMVAR_BLANK
+    EMPTY = :RACTOR_TMVAR_EMPTY
 
     #
-    # TMVar.new(value) to initialize TMVar
+    # initialize TMVar
     #
-    # @param [Object] tvar neet to be shareable
+    # @param [Object] value Value to set TVar. It needs to be shareable.
     #
     # @return [TMVar]
     #
-    def initialize(tvar = nil)
-      @tvar = Ractor::TVar.new(tvar)
+    def initialize(value = nil)
+      @tvar = Ractor::TVar.new(value)
     end
 
     #
-    # Get TVar's value and leave the value to "blank".
-    # If the value is already "blank", it will retry the transaction.
+    # get the value and leave the value to "empty"
+    # If the value is already "empty", it will retry the transaction.
+    # @note You need to wrap it by Ractor.atomically even if you only call +take+
+    #       because +TVar#value=+ needs atomically.
     #
     # @return [Object] value of internal TVar.
     #
-    def value
+    def take
       v = @tvar.value
-      raise Ractor::RetryTransaction if v == BLANK
+      raise Ractor::RetryTransaction if v == EMPTY
 
-      # NOTE: TVar cannot be set without `Ractor.atomically`.
-      Ractor.atomically do
-        @tvar.value = BLANK
-      end
+      @tvar.value = EMPTY
       v
     end
 
     #
-    # Put value to TVar's value.
-    # If the value is not "blank", it will retry the transaction.
+    # try to get the value.
+    # If the value is "empty", it returns nil.
+    #
+    # @return [Object] value of internal TVar, only if exists.
+    #
+    def try_take
+      v = @tvar.value
+      return nil if v == EMPTY
+
+      @tvar.value = EMPTY
+      v
+    end
+
+    #
+    # get the value like +take+.
+    # The difference between +take+ and +read+ is +take+ leaves the value blank,
+    # but +read+ not change the value to blank.
+    #
+    # @return [Object] value of internal TVar.
+    #
+    def read
+      v = @tvar.value
+      raise Ractor::RetryTransaction if v == EMPTY
+
+      v
+    end
+
+    #
+    # read the value like +read+ but it does not retry.
+    #
+    # @return [Object] value of internal TVar.
+    #
+    def try_read
+      v = @tvar.value
+      v == EMPTY ? nil : v
+    end
+
+    #
+    # write the given value.
+    # If the current value is not "empty", it retries the transaction.
     #
     # @param [Object] new_value neet to be shareable
     #
-    def value=(new_value)
-      raise Ractor::RetryTransaction if @tvar.value != BLANK
+    def put(new_value)
+      raise Ractor::RetryTransaction if @tvar.value != EMPTY
 
       @tvar.value = new_value
+    end
+
+    #
+    # try to put value to TVar's value
+    # If the value is not "empty", it will not retry and only return false.
+    # If it succeed to put, it returns true.
+    #
+    # @param [Object] new_value neet to be shareable
+    #
+    def try_put(new_value)
+      return false if @tvar.value != EMPTY
+
+      @tvar.value = new_value
+      true
+    end
+
+    #
+    # return the value is "empty" or not.
+    #
+    # @param [Boolean]
+    #
+    def empty?
+      @tvar.value == EMPTY
+    end
+
+    #
+    # get the the value like +get+, and replace the value to the given value if the current value is not "empty"
+    #
+    # @param [Object] new_value neet to be shareable
+    #
+    def swap(new)
+      v = @tvar.value
+      raise Ractor::RetryTransaction if v == EMPTY
+
+      @tvar.value = new
+      v
     end
   end
 end
--- test/ractor/tmvar_test.rb
+++ test/ractor/tmvar_test.rb
@@ -10,23 +10,38 @@ class Ractor
       end
     end
 
-    test "Ractor::TMVar can has a value" do
+    test "Ractor::TMVar can be taken its value" do
       tv = Ractor::TMVar.new(1)
-      assert_equal 1, tv.value
+      v = nil
+      Ractor.atomically do
+        v = tv.take
+      end
+      assert_equal 1, v
     end
 
     test "Ractor::TMVar without initial value will return nil" do
       tv = Ractor::TMVar.new
-      assert_equal nil, tv.value
+      v = nil
+      Ractor.atomically do
+        v = tv.take
+      end
+      assert_equal nil, v
     end
 
-    test "Ractor::TMVar can change the value" do
+    test "Ractor::TMVar can be changed its value" do
       tv = Ractor::TMVar.new
-      assert_equal nil, tv.value
+      v = nil
       Ractor.atomically do
-        tv.value = :ok
+        v = tv.take
       end
-      assert_equal :ok, tv.value
+      assert_equal nil, v
+      Ractor.atomically do
+        tv.put :ok
+      end
+      Ractor.atomically do
+        v = tv.take
+      end
+      assert_equal :ok, v
     end
 
     test "Ractor::TMVar can not set the unshareable value" do
@@ -43,18 +58,80 @@ class Ractor
           value1 = nil
           value2 = nil
           Ractor.atomically do
-            value1 = v1.value
-            value2 = v2.value
+            value1 = v1.take
+            value2 = v2.take
           end
           Ractor.atomically do
-            v2.value = value2 + 2
-            v1.value = value1 + 1
+            v2.put value2 + 2
+            v1.put value1 + 1
           end
         end
       end
       rs.each(&:take)
-      assert_equal 100, tv1.value
-      assert_equal 200, tv2.value
+      tv1_value = nil
+      tv2_value = nil
+      Ractor.atomically do
+        tv1_value = tv1.take
+        tv2_value = tv2.take
+      end
+      assert_equal 100, tv1_value
+      assert_equal 200, tv2_value
+    end
+
+    test "Ractor::TMVar can only read the value" do
+      tv = Ractor::TMVar.new(1)
+      assert_equal 1, tv.read
+    end
+
+    test "Ractor::TMVar returns nil when try_read to EMPTY TMVar" do
+      tv = Ractor::TMVar.new(1)
+      assert_equal 1, tv.read
+      tv = Ractor::TMVar.new(Ractor::TMVar::EMPTY)
+      assert_equal nil, tv.try_read
+    end
+
+    test "Ractor::TMVar returns nil when try_take to EMPTY TMVar" do
+      tv = Ractor::TMVar.new(Ractor::TMVar::EMPTY)
+      v = nil
+      Ractor.atomically do
+        v = tv.try_take
+      end
+      assert_equal nil, v
+    end
+
+    test "Ractor::TMVar returns true/false when try_put to EMPTY/not-EMPTY TMVar" do
+      tv = Ractor::TMVar.new
+      v = true
+      Ractor.atomically do
+        v = tv.try_put(30)
+      end
+      assert_equal false, v
+      tv = Ractor::TMVar.new(Ractor::TMVar::EMPTY)
+      v = false
+      Ractor.atomically do
+        v = tv.try_put(30)
+      end
+      assert_equal true, v
+    end
+
+    test "Ractor::TMVar can report its value is now empty or not" do
+      tv = Ractor::TMVar.new(Ractor::TMVar::EMPTY)
+      assert_equal true, tv.empty?
+      tv = Ractor::TMVar.new
+      assert_equal false, tv.empty?
+    end
+
+    test "Ractor::TMVar can swap its value" do
+      tv = Ractor::TMVar.new(10)
+      v = nil
+      Ractor.atomically do
+        v = tv.swap(20)
+      end
+      assert_equal 10, v
+      Ractor.atomically do
+        v = tv.take
+      end
+      assert_equal 20, v
     end
   end
 end
